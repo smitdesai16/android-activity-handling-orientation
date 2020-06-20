fa50e2fe868a47a53a1d6a2bbdf8ab666f263e23
Smit Desai <smit.desai16@gmail.com>
2020-06-20 13:47:53 -0500

    Issue with orientation

--- app/src/main/java/me/creatorguy/androidactivityhandlingorientation/MainActivity.java
+++ app/src/main/java/me/creatorguy/androidactivityhandlingorientation/MainActivity.java
@@ -3,12 +3,36 @@ package me.creatorguy.androidactivityhandlingorientation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static Button btnMain;
+    private static TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMain = findViewById(R.id.btnMain);
+        tvMain = findViewById(R.id.tvMain);
+
+        btnMain.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnMain:
+                btnMain.setText("Updated");
+                tvMain.setText("Updated");
+                break;
+
+            default:
+                break;
+        }
     }
 }
